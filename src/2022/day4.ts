78363b89073af4d539149fe69b4bbad3a5738b36
Chris Leveille <cdleveille@gmail.com>
2022-12-04 01:08:42 -0500

    simplification

--- src/2022/day4.ts
+++ src/2022/day4.ts
@@ -7,33 +7,26 @@ export const day4 = () => {
 		return value >= min && value <= max;
 	};
 
-	// part 1
-	const part1Count = day4Input.reduce((count, rangePair) => {
-		const [leftRange, rightRange] = rangePair.split(",");
-		const [leftRangeMin, leftRangeMax] = leftRange.split("-").map(val => parseInt(val));
-		const [rightRangeMin, rightRangeMax] = rightRange.split("-").map(val => parseInt(val));
-		if (
-			(leftRangeMin >= rightRangeMin && leftRangeMax <= rightRangeMax) ||
-			(rightRangeMin >= leftRangeMin && rightRangeMax <= leftRangeMax)
-		)
-			return count + 1;
-		return count;
-	}, 0);
-	console.log(`Part 1: ${part1Count}`);
-
-	// part 2
-	const part2Count = day4Input.reduce((count, rangePair) => {
-		const [leftRange, rightRange] = rangePair.split(",");
-		const [leftRangeMin, leftRangeMax] = leftRange.split("-").map(val => parseInt(val));
-		const [rightRangeMin, rightRangeMax] = rightRange.split("-").map(val => parseInt(val));
-		if (
-			isInRange(leftRangeMin, rightRangeMin, rightRangeMax) ||
-			isInRange(leftRangeMax, rightRangeMin, rightRangeMax) ||
-			isInRange(rightRangeMin, leftRangeMin, leftRangeMax) ||
-			isInRange(rightRangeMax, leftRangeMin, leftRangeMax)
-		)
-			return count + 1;
-		return count;
-	}, 0);
-	console.log(`Part 2: ${part2Count}`);
+	const { part1Count, part2Count } = day4Input.reduce(
+		(count, rangePair) => {
+			const [leftRange, rightRange] = rangePair.split(",");
+			const [leftRangeMin, leftRangeMax] = leftRange.split("-").map(val => parseInt(val));
+			const [rightRangeMin, rightRangeMax] = rightRange.split("-").map(val => parseInt(val));
+			if (
+				(leftRangeMin >= rightRangeMin && leftRangeMax <= rightRangeMax) ||
+				(rightRangeMin >= leftRangeMin && rightRangeMax <= leftRangeMax)
+			)
+				count.part1Count += 1;
+			if (
+				isInRange(leftRangeMin, rightRangeMin, rightRangeMax) ||
+				isInRange(leftRangeMax, rightRangeMin, rightRangeMax) ||
+				isInRange(rightRangeMin, leftRangeMin, leftRangeMax) ||
+				isInRange(rightRangeMax, leftRangeMin, leftRangeMax)
+			)
+				count.part2Count += 1;
+			return count;
+		},
+		{ part1Count: 0, part2Count: 0 }
+	);
+	console.log(`Part 1: ${part1Count}\nPart 2: ${part2Count}`);
 };
